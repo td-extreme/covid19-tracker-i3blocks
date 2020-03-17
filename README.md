60499d23e9e7b58f5cb9c33c8dfdb8ddd9da521c
Tyler Decker <tdecker@8thlight.com>
2020-03-16 20:01:37 -0500

    add script

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # covid19-tracker-i3blocks
+
+This for adding a Covid 19 case and death count for UK and World Wide to the i3blocks status bar. I was made in response to this [reddit post](https://www.reddit.com/r/i3wm/comments/fjrqyh/i3blocks_idea_for_corona_virus_web_scraper/).
+
+I took the orginal script [found here] (https://pastebin.com/tFZK5vpy) and changed it so the updates are written to a file.  
+
+To use this add this to your i3blocks file and the covid19status.py script to your startup
+
+```
+[covid19]
+command=inotifywait -qq -e close_write ~/Temp/covid19-count-i3blocks/log.txt; echo $(tail -1 ~/Temp/covid19-count-i3blocks/log.txt)
+interval=repeat
+```
+
+
+
+
+
---
+++ covid19stats.py
@@ -0,0 +1,95 @@
+import time
+import re
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+def getStats():
+    url = "https://www.worldometers.info/coronavirus/country/uk/"
+    url2 = "https://www.worldometers.info/coronavirus/"
+
+    # Get response from first URL which contains data about UK cases and deaths
+    print("[!] Getting response from URL 1")
+    startTime = time.time()
+    response = requests.get(url)
+    print("[!] Time taken to retrieve response", round((time.time() - startTime), 2))
+
+    # Get response from second URL which contains worldwide cases and deaths
+    print("[!] Getting response from URL 2")
+    startTime = time.time()
+    response2 = requests.get(url2)
+    print("[!] Time taken to retrieve response", round((time.time() - startTime), 2))
+
+    # Use BeautifulSoup library to parse the URLs we just retrieved
+    soup = BeautifulSoup(response.text, "html.parser")
+    soup2 = BeautifulSoup(response2.text, "html.parser")
+
+    # Create empty array called stats
+    stats = []
+
+    # For each statement to catch all lines of code that contain h1 tag
+    for el in soup.findAll('h1'):
+        if "Coronavirus" in str(el):
+            stats.append(re.sub('<[^<]+?>', '', str(el.find_next_sibling())).replace(" ", ""))
+        elif "Deaths" in str(el):
+            stats.append(re.sub('<[^<]+?>', '', str(el.find_next_sibling())).replace(" ", ""))
+
+    for el in soup2.findAll('h1'):
+        if "Coronavirus" in str(el):
+            stats.append(re.sub('<[^<]+?>', '', str(el.find_next_sibling())).replace(" ", ""))
+        if "Deaths" in str(el):
+            stats.append(re.sub('<[^<]+?>', '', str(el.find_next_sibling())).replace(" ", ""))
+
+    return stats
+
+
+def main():
+    difference = [0] * 4
+
+    while True:
+
+        try:
+            oldstats = newstats
+
+        except:
+            print("start")
+
+        try:
+            newstats = getStats()
+
+        except Exception as e:
+            print("[!] Error retrieving stats; website most likely denied request. Reattempting in 60 seconds")
+            time.sleep(60)
+            try:
+                newstats = getStats()
+            except Exception as e:
+                print("[!] Error retrieving second time; waiting 2m50s then running again")
+                time.sleep(150)
+                continue
+
+        try:
+            difference[0] += (int(re.sub("\D", "", newstats[0])) - int(re.sub("\D", "", oldstats[0])))
+            difference[1] += (int(re.sub("\D", "", newstats[1])) - int(re.sub("\D", "", oldstats[1])))
+            difference[2] += (int(re.sub("\D", "", newstats[2])) - int(re.sub("\D", "", oldstats[2])))
+            difference[3] += (int(re.sub("\D", "", newstats[3])) - int(re.sub("\D", "", oldstats[3])))
+
+        except Exception as e:
+            print("[!] Variable old stats does not exist yet; most likely first run")
+
+        try:
+            print("[*] Updated stats at:", datetime.datetime.now())
+            f = open("log.txt", "a")
+            f.write("\nUK C: " + newstats[0] + " (+" + str(difference[0]) + ")" +
+            " UK D: " + newstats[1] + " (+" + str(difference[1]) + ")" +
+            " WW C: " + newstats[2] + " (+" + str(difference[2]) + ")" +
+            " WW D: " + newstats[3] + " (+" + str(difference[3]) + ")")
+            f.close()
+
+        except Exception as e:
+            print("[!] Error:", e)
+            continue
+
+        time.sleep(300)
+
+if __name__ == '__main__':
+    main()
