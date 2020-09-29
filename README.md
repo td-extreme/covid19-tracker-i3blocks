ec890975f848014c2ca1289e626b9aa44ff7b653
c-tydecker <c_tydecker@groupon.com>
2020-09-29 17:33:24 -0500

    remove label param

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 # covid19-tracker-i3blocks
 
-This for adding a Covid 19 case and death count for UK and World Wide to the i3blocks status bar. I was made in response to this [reddit post](https://www.reddit.com/r/i3wm/comments/fjrqyh/i3blocks_idea_for_corona_virus_web_scraper/).
+Add a Covid 19 case and death count to the i3blocks status bar. I was made in response to this [reddit post](https://www.reddit.com/r/i3wm/comments/fjrqyh/i3blocks_idea_for_corona_virus_web_scraper/).
 
 I took the orginal script [found here] (https://pastebin.com/tFZK5vpy) and changed it so that it no longer calculates the addititonal cases or deaths that occured since the last refresh. (5 minutes)
-
 
 i3 examples
 
 ```
 $SCRIPT = path/to/script/folder/
 [covid19-ww]
-command=$SCRIPT/covid19-count-i3blocks/covid19stats.py ww 
+label=World Wide
+command=$SCRIPT/covid19-count-i3blocks/covid19stats.py
 interval=3600
 
 [covid19-uk]
-command=$SCRIPT/covid19-count-i3blocks/covid19stats.py uk U.K.
+label=U.K.
+command=$SCRIPT/covid19-count-i3blocks/covid19stats.py uk
 interval=3600
 
 [covid19-china]
-command=$SCRIPT/covid19-count-i3blocks/covid19stats.py china China
+label=China
+command=$SCRIPT/covid19-count-i3blocks/covid19stats.py china
 interval=3600
 ```
--- covid19stats.py
+++ covid19stats.py
@@ -5,13 +5,12 @@ import requests
 import argparse
 from bs4 import BeautifulSoup
 from optparse import OptionParser
- 
-refreshRate = 300
+
 baseUrl = "https://www.worldometers.info/coronavirus/"
 country = "country/"
 
 def getStatsForCountry(countryCode):
-    if not countryCode or countryCode == "ww":
+    if not countryCode:
         return getStatusForUrl(baseUrl)
     return getStatusForUrl(baseUrl + country + countryCode)
 
@@ -22,33 +21,22 @@ def getStatusForUrl(url):
     infection_count = death_count = -1
     for el in soup.findAll('h1'):
         if "Coronavirus" in str(el):
-            infection_count = re.sub("\D", "", re.sub('<[^<]+?>', '', str(el.find_next_sibling())).replace(" ", "")).replace("\n", "")
+            infection_count = re.sub('<[^<]+?>', '', str(el.find_next_sibling()).replace(" ", "")).replace("\n", "")
         elif "Deaths" in str(el):
-            death_count = re.sub("\D", "", re.sub('<[^<]+?>', '', str(el.find_next_sibling())).replace(" ", "")).replace("\n", "")
+            death_count = re.sub('<[^<]+?>', '', str(el.find_next_sibling()).replace(" ", "")).replace("\n", "")
 
     return (infection_count, death_count)
 
-def write_stats(label, infection_count, death_count):
-    if not label:
-      label = ""
-    return "{0} c: {1} d: {2} ".format(label, infection_count, death_count)
-
 def main():
-    parser = argparse.ArgumentParser(description='Optional label countryCode')
+    parser = argparse.ArgumentParser(description='Optional countryCode')
     parser.add_argument('country', type=str, nargs='?',
                         help='country code')
-    parser.add_argument('label', type=str, nargs='?',
-                        help='label')
 
     args = parser.parse_args()
-    if args.label:
-        label = args.label
-    if not args.label and args.country:
-        label = args.country
-    
+
     try:
         (infection_count, death_count) = getStatsForCountry(args.country)
-        print(write_stats(label, infection_count, death_count))
+        print(f" c: {infection_count} d: {death_count}")
     except Exception as e:
         print("[!] Error:", e)
 
